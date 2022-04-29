1bc809e3f6d28cfff94ff27021629285e92d9b9e
NIcholas montes da silva <nicholasmontes0@gmail.com>
2022-04-29 14:43:37 -0300

    Implement Phonebook

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,4 +3,44 @@ class ContactsController < ApplicationController
   def index
     @contacts = Contact.all
   end
+  
+  def show
+    @contact = Contact.find(params[:id])
+  end
+
+  def new
+    @contact = Contact.new
+    @contact.build_address
+  end
+  
+  def edit
+    @contact = Contact.find(params[:id])
+  end
+
+  def update
+    @contact = Contact.find(params[:id])
+    if @contact.update(contact_params)
+      redirect_to action: :show
+    end
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to action: :index
+    end
+  end
+
+  def destroy
+    @contact = Contact.find(params[:id])
+    if @contact.destroy
+      redirect_to action: :index
+    end
+  end
+
+  private
+    def contact_params
+      params.require(:contact).permit(:name, :phone, :birthdate, address_attributes:[:city, :number, :street])
+    end
+
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,4 @@
 class Contact < ApplicationRecord
     belongs_to :address
+    accepts_nested_attributes_for :address
 end
